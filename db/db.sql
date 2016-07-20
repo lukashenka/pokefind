b26f82c045d212852f1475f53a94510e9882fde4
e.karachun <e.karachun@rozumsoft.by>
2016-07-20 12:48:57 +0300

    Hui

--- db/db.sql
+++ db/db.sql
@@ -23,3 +23,14 @@ CREATE TABLE pokemon_location
 CREATE INDEX pokemon_created ON pokemon_location (created);
 CREATE INDEX pokemon_id ON pokemon_location (pokemon_id);
 CREATE INDEX pokemon_location_lat_lng_index ON pokemon_location (lat, lng);
+
+CREATE TABLE location_for_update
+(
+    id INT(11) PRIMARY KEY NOT NULL,
+    lat FLOAT(10,6),
+    lng FLOAT(10,6),
+    blocked TINYINT(1) DEFAULT '0',
+    created DATETIME DEFAULT '0000-00-00 00:00:00' NOT NULL
+);
+CREATE INDEX created ON location_for_update (created, blocked);
+CREATE UNIQUE INDEX location_for_update_lat_lng_uindex ON location_for_update (lat, lng);
